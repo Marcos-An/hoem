c9256321c2fd6ad86442e8a7c7b1a2d5007df47c
Marcos-An <loseonjr@gmail.com>
2020-01-28 11:57:35 -0300

    fix: route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/api', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).send({
     itle: 'api de imóves hoem, use a vontade :D'
   });
