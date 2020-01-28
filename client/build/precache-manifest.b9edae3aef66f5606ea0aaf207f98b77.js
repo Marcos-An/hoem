8a96102392a341a972056405745f627a18ab3d65
Marcos-An <loseonjr@gmail.com>
2020-01-28 10:38:37 -0300

    Form and Cards

---
+++ client/build/precache-manifest.b9edae3aef66f5606ea0aaf207f98b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344246453bbae8882c2dfbbb10621a7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb347d4adc9bcdb1e0fd",
+    "url": "/static/css/main.35a4c618.chunk.css"
+  },
+  {
+    "revision": "6165ea6f11c6538ddcfc",
+    "url": "/static/js/2.504d282e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.504d282e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb347d4adc9bcdb1e0fd",
+    "url": "/static/js/main.df274723.chunk.js"
+  },
+  {
+    "revision": "eda5874fecf55dfecd04",
+    "url": "/static/js/runtime-main.4d7b17b4.js"
+  }
+]);
